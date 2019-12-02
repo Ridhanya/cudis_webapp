546c04b84d7b1b820665c4c7630c4099660fa96f
Ridhanya <ridhanyasree.balamurugan@gmail.com>
2019-12-02 16:42:15 +0530

    new

--- opencv/about_us_process.py
+++ opencv/about_us_process.py
@@ -1,6 +1,7 @@
 import cv2
  
-img = cv2.imread('C:\\Users\\Bala Murugan\\Desktop\\tea\\rid\\CuDIS_webpage\\cudis\\static\\images\\Anubhav2.jpg', cv2.IMREAD_UNCHANGED)
+img = cv2.imread('C:\\Users\\Admin\\Desktop\\cudis\\web\\cudis\\static\\images\\rgk.jpg', cv2.IMREAD_UNCHANGED)
+
 #cv2.imshow("img",img)
  
 #print('Original Dimensions : ',img.shape())
@@ -8,13 +9,14 @@ img = cv2.imread('C:\\Users\\Bala Murugan\\Desktop\\tea\\rid\\CuDIS_webpage\\cud
 scale_percent = 60 # percent of original size
 #width = int(img.shape[1] * scale_percent / 100)
 #height = int(img.shape[0] * scale_percent / 100)
-dim = (555, 693)
+dim = (300,400)
 # resize image
 resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+
  
 #print('Resized Dimensions : ',resized.shape)
  
 #cv2.imshow("Resized image", resized)
-cv2.imwrite('C:\\Users\\Bala Murugan\\Desktop\\tea\\rid\\CuDIS_webpage\\cudis\\static\\images\\Anubhav2_resized.jpg',resized)
+cv2.imwrite('C:\\Users\\Admin\\Desktop\\cudis\\web\\cudis\\static\\images\\rgk_resized.jpg',resized)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
